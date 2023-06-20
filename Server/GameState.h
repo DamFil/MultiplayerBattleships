97ec0010c611753cf7077f9e89f247a5f84b14c0
DamFil <damjan.filipovic.21@um.edu.mt>
2023-06-20 12:10:36 +0200

    Added a class for managing synchronised accesses to the global server variables

--- Server/GameState.h
+++ Server/GameState.h
@@ -1,13 +1,45 @@
 #include <vector>
 #include <mutex>
+#include <condition_variable>
+#include <bits/stdc++.h>
+#include "Player.h"
 
 using namespace std;
 
-mutex m;
-
-int num_players = 0;
-int num_spectators = 0;
-
 bool start_game = false;
-// attacks made by each player
-vector<vector<pair<int, int>>> player_attempts{};
+
+class GameState
+{
+private:
+    mutex m;
+    int num_players;
+    int num_spectators;
+    vector<Player *> active_players{};
+
+public:
+    GameState() : num_players(0), num_spectators(0) {}
+
+    void addPlayer(Player *p)
+    {
+        lock_guard<mutex> l(this->m);
+        active_players.push_back(p);
+    }
+
+    void removePlayer(Player *p)
+    {
+        lock_guard<mutex> l(this->m);
+        remove(active_players.begin(), active_players.end(), p);
+    }
+
+    void incPlayers(int amnt = 1)
+    {
+        lock_guard<mutex> l(this->m);
+        num_players += amnt;
+    }
+
+    void decPlayers()
+    {
+        lock_guard<mutex> l(this->m);
+        --num_players;
+    }
+};
